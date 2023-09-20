7c909960dc99fac7b0d792b134552b74023cde79
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-20 16:49:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int pieces = width * length; 
+                                         
+            while (pieces >= 0)
+            {
+                String input = Console.ReadLine();
+                if (input == "STOP")
+                    break;
+
+                pieces -= int.Parse(input);
+            }
+
+            if (pieces >= 0)
+                Console.WriteLine($"{pieces} pieces are left.");
+            else
+                Console.WriteLine($"No more cake left! You need {-pieces} pieces more.");
+        }
+    }
+}
